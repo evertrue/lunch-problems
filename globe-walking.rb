5e83f576c0a457239223034ec76e92085d71f287
Bob Breznak <bob.breznak@gmail.com>
2014-11-19 12:39:46 -0500

    Update globe-walking.rb

--- globe-walking.rb
+++ globe-walking.rb
@@ -1,5 +1,6 @@
-  # Globe Walking
-  
-  How many points are there on the globe where, by walking one mile south, then one mile east and then one mile north, you would reach the place where you started?
-  
-  Found at: [MY TECH INTERVIEWS](http://www.mytechinterviews.com/10-famous-microsoft-interview-puzzles)
+# Globe Walking
+_Asked 11/19/2014_
+
+How many points are there on the globe where, by walking one mile south, then one mile east and then one mile north, you would reach the place where you started?
+
+Found at: [MY TECH INTERVIEWS](http://www.mytechinterviews.com/10-famous-microsoft-interview-puzzles)
